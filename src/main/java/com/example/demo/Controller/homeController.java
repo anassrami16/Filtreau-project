c0a40dabd67b30113c44a602795ec380fd956569
astro <astro@DESKTOP-T170C4P>
2020-09-30 04:23:12 +0100

    init commit

---
+++ src/main/java/com/example/demo/Controller/homeController.java
@@ -0,0 +1,45 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Card.Product;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@Scope("session")
+@SessionAttributes({"card"})
+public class homeController {
+
+    @GetMapping(path = "/")
+    public String home(){
+        return "home";
+    }
+    @GetMapping(path = "/product")
+    public String product(){
+        return "product";
+    }
+    @GetMapping(path = "/sandd")
+    public String sandd(){
+        return "sandd";
+    }
+    @GetMapping(path = "/contactus")
+    public String contactus(){
+        return "contactus";
+    }
+
+    @GetMapping(path = "/checkout")
+    public String checkout(@RequestParam int quantity , Model model){
+        if (quantity != 0){
+            Product p1=new Product("Filtreau - Systeme d'osmose inverse 7 étape",quantity,2300);
+            model.addAttribute("SimpleCard",p1);
+            return "checkout";
+        }
+        else {
+            return "redirect:product";
+        }
+
+    }
+}
